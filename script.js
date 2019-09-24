c619ddcc9b88c54c5159bb8468cf7f0f52dcdaea
Jacoby Broadnax <32787662+MashPop@users.noreply.github.com>
2019-09-24 16:38:13 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+       window.setTimeout(loaded, 3700);
+
+        var element = document.getElementById('loader');
+
+        function loaded() {
+            element.style.display = 'none';
+        }
+
+        function image1(){
+            document.getElementById("image-input-1").value = "Testing"
+        }
+      
